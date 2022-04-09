d82be880c5bae17a9942ebeae310c34c13c2b7f0
Yutong1008 <87843780+Yutong1008@users.noreply.github.com>
2022-04-09 12:24:52 -0400

    jjj

--- search/src/main/java/com/example/search/controller/SearchController.java
+++ search/src/main/java/com/example/search/controller/SearchController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-
+//ddddffffffbnyyh4
 @RestController
 public class SearchController {
 
